e99c888e9e124a62cc5c4f6dcf491e82fac26573
Ankita Binu <35870562+ankitabinu1998@users.noreply.github.com>
2023-03-07 10:49:33 -0500

    rearranged edit component

--- src/components/BookEdit.js
+++ src/components/BookEdit.js
@@ -1,43 +1,22 @@
+
+import Form from 'react-bootstrap/Form'
 import { useState } from "react";
 import Button from 'react-bootstrap/Button';
-import Form from 'react-bootstrap/Form'
 
 export default function BookEdit (props) {
-    const {setBooksList,book,booksList} = props;
-    const [isEdit,setIsEdit] = useState(false);
-    const [newTitle, setNewTitle] = useState('');
-    const onEditBook = () => {
-        setIsEdit(!isEdit);
-    }
-    const onSubmitNewTitle = (event) => {
-        event.preventDefault();;
-        const newBooksList = booksList.map(bookInList => {
-            if (bookInList.id === book.id) {
-                return {...bookInList,title:newTitle};
-            }
-            return bookInList;
-        })
-        console.log('newBooksList',newBooksList);
-        console.log('oldBooksList',booksList);
-        // setBooksList(newBooksList);
+    const {addNewTitle,book} = props;
+    const [newTitle, setNewTitle] = useState(book.title);
+
+    const onSubmitNewTitle = ((event) => {
+        event.preventDefault();
+        addNewTitle(newTitle);
         setNewTitle('');
-        setIsEdit(!isEdit);
-    }
-    const onDeleteBook = () => {
-        setBooksList(booksList.filter(bookInList => bookInList.id !== book.id));
-        setIsEdit(false);
-    }
+    });
 
     return (
-        <div>
-            <Button className="me-1" variant="secondary" onClick={onEditBook}><i className="icon-edit"></i></Button>
-            <Button variant="light" onClick={onDeleteBook}>x</Button>
-            {isEdit &&
-                <form onSubmit={onSubmitNewTitle}>
-                    <Form.Control className="mb-2 mt-2"  type="text" value={newTitle} onChange={(e)=>setNewTitle(e.target.value)}/>
-                    <Button  variant='secondary' type="submit">Save</Button>
-                </form>
-            }
-        </div>
+        <form onSubmit={onSubmitNewTitle}>
+            <Form.Control className="mb-2"  type="text" value={newTitle} onChange={(e)=>setNewTitle(e.target.value)}/>
+            <Button  variant='secondary' type="submit">Save</Button>
+        </form>
     )
 }
--- src/components/BookShow.js
+++ src/components/BookShow.js
@@ -1,14 +1,49 @@
 import BookEdit from './BookEdit'
 import Card from 'react-bootstrap/Card';
+import Button from 'react-bootstrap/Button';
+import { useState } from "react";
 export default function BookShow (props) {
     const {book, setBooksList, booksList} = props;
+    const [isEdit,setIsEdit] = useState(false);
+    const onEditBook = () => {
+        setIsEdit(!isEdit);
+    }
+    const addNewTitle = (newTitle) => {
+        const newBooksList = booksList.map(bookInList => {
+            if (bookInList.id === book.id) {
+                return {...bookInList,title:newTitle};
+            }
+            return bookInList;
+        })
+        setBooksList(newBooksList);
+        setIsEdit(!isEdit);
+    }
+    const onDeleteBook = () => {
+        setBooksList(booksList.filter(bookInList => bookInList.id !== book.id));
+        setIsEdit(false);
+    }
+    const renderedBook = () => {
+        return (
+            <Card.Body className='d-flex justify-content-between'>
+            <Card.Title className='text-capitalize'>{book.title}</Card.Title>
+            <div>
+                <Button className="me-1" variant="secondary" onClick={onEditBook}><i className="icon-edit"></i></Button>
+                <Button variant="light" onClick={onDeleteBook}>x</Button>
+            </div>
+            </Card.Body>
+            )
+    }
+    const renderedForm = () => {
+        return (
+            <Card.Body>
+            <BookEdit book={book} addNewTitle={addNewTitle}/>
+            </Card.Body>
+            )
+    }
     return (
         <Card style={{ width: '20rem' }}>
             <Card.Img variant="top" src="https://media.istockphoto.com/id/1392740267/vector/open-book-line-art.jpg?s=612x612&w=0&k=20&c=spk1njg8DdUNny_XIqVsXsBrDLjarRkLibE3mqYeMqA="/> 
-            <Card.Body className='d-flex justify-content-between'>
-                <Card.Title className='text-capitalize'>{book.title}</Card.Title>
-                <BookEdit book={book} setBooksList={setBooksList} booksList={booksList}/>
-            </Card.Body>
+            {isEdit ? renderedForm() : renderedBook()}
         </Card>
     )
 }
